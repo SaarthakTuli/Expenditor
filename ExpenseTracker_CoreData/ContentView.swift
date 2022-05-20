b980fd47d43ad78e569396139a96833bea2be959
SaarthakTuli <saarthaktuli@gmail.com>
2022-05-20 17:49:16 +0530

    Initial Commit

---
+++ ExpenseTracker_CoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExpenseTracker_CoreData
+//
+//  Created by Saarthak Tuli on 20/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExpenseTracker_CoreData/ExpenseTracker_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTracker_CoreDataApp.swift
+//  ExpenseTracker_CoreData
+//
+//  Created by Saarthak Tuli on 20/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTracker_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
