3721cb5a1cdfc572eb905595a48463e564e1766c
SaarthakTuli <saarthaktuli@gmail.com>
2022-12-29 00:27:58 +0530

    Initial Commit

---
+++ Expenditor/ExpenditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenditorApp.swift
+//  Expenditor
+//
+//  Created by Saarthak Tuli on 29/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
